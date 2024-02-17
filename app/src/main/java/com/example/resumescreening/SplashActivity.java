8811bbe2b9a22b2b1ec30dffebe876ab7434f190
Mehedi Hasan Sabbir <mehedihasansabbirstudent8@gmail.com>
2024-02-17 19:16:26 +0600

    SignUp+Login

---
+++ app/src/main/java/com/example/resumescreening/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.resumescreening;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        /* ***** Create Thread that will sleep for 5 seconds*** */
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(10*1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                finally{
+                    // After 5 seconds redirect to another intent
+                    Intent intent=new Intent(SplashActivity.this,SignUpActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        // start thread
+        thread.start();
+    }
+}
